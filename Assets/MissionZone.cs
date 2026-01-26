651dabb717307f52f926d0c7cced7a7c291a0715
Ha Huy Hoang <se180557hahuyhoang@gmail.com>
2026-01-26 14:38:03 +0700

    Boi canh 1

---
+++ Assets/MissionZone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MissionZone : MonoBehaviour
+{
+    public bool laDiemGiaoHang = false; // Tích vào nếu đây là điểm Đích
+    public ShipperManager quanLyGame; // Kéo GameManager vào đây
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!laDiemGiaoHang && !quanLyGame.dangGiaoHang)
+            {
+                quanLyGame.NhanDonHang(); // Chạm điểm A -> Nhận đơn
+            }
+            else if (laDiemGiaoHang && quanLyGame.dangGiaoHang)
+            {
+                quanLyGame.HoanThanhDonHang(); // Chạm điểm B -> Xong đơn
+            }
+        }
+    }
+}
